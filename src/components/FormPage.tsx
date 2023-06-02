3663c2c2df4050e7b9c59ce0677d2aa50b1e43db
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-03 02:06:11 +0530

    jg: from page

---
+++ src/components/FormPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CarouselContainer from "./CarouselContainer";
+import FormContainer from "./FormContainer";
+
+const FormPage = () => {
+  return (
+    <div className="layout ">
+      <CarouselContainer />
+      <FormContainer />
+    </div>
+  );
+};
+
+export default FormPage;
